2f4aed362d76c243284f27ea4e1e733206c0cb8e
Krishna-Pydipalli <ckrishnapydipalli@gmail.com>
2024-10-20 23:18:43 +0530

    Shop Me

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+import './App.css'
+import MainPage from './ShopMe/pages/MainPage'
+
+const App = () => {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/ShopMe/pages/MainPage.jsx
@@ -0,0 +1,32 @@
+
+
+import React, {useState} from 'react'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+
+import Footer from '../components/Footer'
+
+import {Gents, Ladies} from '../data'
+import MenCollection from '../components/MenCollection'
+import WomenCollection from '../components/WomenCollection'
+
+
+const MainPage = () => {
+
+  const [gentsFashion, setGentsFashion] = useState(Gents)
+  const [ladiesFashion, setLadiesFashion] = useState(Ladies)
+
+  console.log(Gents)
+
+  return (
+    <div>
+        <Header />
+        <Banner />
+        <MenCollection gentsFashion= {gentsFashion}/>
+        <WomenCollection ladiesFashion= {ladiesFashion}/>
+        <Footer />
+    </div>
+  )
+}
+
+export default MainPage
